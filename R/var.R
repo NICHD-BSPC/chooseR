b67ca3eaabd1ec476cfcc4004d6ee093a13aee3f
Mira  Sohn <sohnm@helix.nih.gov>
2021-09-28 22:37:06 -0400

    R/var.R - path updated

--- R/var.R
+++ R/var.R
@@ -1,8 +1,8 @@
 # Source pre-defined variables and functions
-source("/data/BSPC-Petros/multiome-mira/chooser/R/pipeline.R")   # written by chooseR developers
+source("../R/pipeline.R")   # written by chooseR developers
 
 # Input RDS file
-rds_path <- "/data/BSPC-Petros/multiome-mira/results/qc/toydataset.seurat_qc.rds"
+rds_path <- "../../results/qc/toydataset.seurat_qc.rds"
 
 # Clustering resolution range
 resolutions <- c(0.8, 1, 1.2, 1.6, 2, 4, 6, 8, 12, 16)
